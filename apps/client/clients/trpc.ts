721d83b97274a532b56a1d2ef29f9371a54d2059
olaatgit <olanrewajuoladejo08@gmail.com>
2025-06-22 01:38:22 +0000

    Jun 22, 2025, 2:38 AM

--- apps/client/clients/trpc.ts
+++ apps/client/clients/trpc.ts
@@ -1,14 +1,17 @@
 import { trpc } from '@/lib/trpc';
 import { httpBatchLink } from '@trpc/client';
 
-if (!process.env.EXPO_PUBLIC_API_URL) {
-  throw new Error('Missing API URL');
-}
+// if (!process.env.EXPO_PUBLIC_API_URL) {
+//   throw new Error('Missing API URL');
+// }
 
 export const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: `${process.env.EXPO_PUBLIC_API_URL}/api/trpc/`,
+      url: `${
+        process.env.EXPO_PUBLIC_API_URL ||
+        'https://estimate-server.onrender.com'
+      }/api/trpc`,
       fetch(url, options) {
         return fetch(url, {
           ...options,
