36683251a7b14867e87e43da94e60efc06717acc
olaatgit <olanrewajuoladejo08@gmail.com>
2025-06-22 01:36:25 +0000

    Jun 22, 2025, 2:36 AM

--- apps/client/clients/trpc.ts
+++ apps/client/clients/trpc.ts
@@ -1,10 +1,14 @@
 import { trpc } from '@/lib/trpc';
 import { httpBatchLink } from '@trpc/client';
 
+if (!process.env.EXPO_PUBLIC_API_URL) {
+  throw new Error('Missing API URL');
+}
+
 export const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: `${process.env.EXPO_PUBLIC_API_URL}/api/trpc`,
+      url: `${process.env.EXPO_PUBLIC_API_URL}/api/trpc/`,
       fetch(url, options) {
         return fetch(url, {
           ...options,
