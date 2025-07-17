fd801d3d4540c942821d7957bea5369b9455b08a
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-17 21:06:04 +0000

    Jul 17, 2025, 10:06 PM

--- apps/client/features/auth/hooks/useRegister.ts
+++ apps/client/features/auth/hooks/useRegister.ts
@@ -2,11 +2,15 @@ import { router } from 'expo-router';
 import { Alert } from 'react-native';
 import { Popup } from '@/components';
 import { Trpc } from '@/features/trpc';
+import { useForm } from 'react-hook-form';
 
 export const useRegister = () => {
+  const form = useForm();
   const popup = Popup.usePopup();
+
   const register = Trpc.client.auth.public.register.useMutation({
     onSuccess: () => {
+      form.reset();
       popup.open({
         variant: 'success',
         title: 'Congratulations, your account is now active.',
