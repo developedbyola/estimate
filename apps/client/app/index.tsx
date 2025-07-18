41825e970e84a003113aa47be33ea3a851396611
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-18 00:24:27 +0000

    Jul 18, 2025, 1:24 AM

--- apps/client/app/index.tsx
+++ apps/client/app/index.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'expo-router';
 import { Border, Space } from '@/constants';
 import { useThemeColors } from '@/hooks/useThemeColors';
 import { Action, Heading, Box, Text, Safe, Overlay } from '@/components';
+import { Auth } from '@/features/auth';
 // import { useDeleteAccessToken } from '@/hooks/useTokens';
 
 const images = [
@@ -27,8 +28,17 @@ const images = [
 
 const Index = () => {
   const router = useRouter();
+  const { auth } = Auth.useAuth();
   const colors = useThemeColors();
   // useDeleteAccessToken();
+
+  React.useEffect(() => {
+    if (auth.refreshToken) {
+      router.replace('/onboard');
+    }
+  }, [auth.refreshToken]);
+
+  console.log({ auth });
 
   return (
     <Box
