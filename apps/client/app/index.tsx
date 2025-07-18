3f9401cfa4156eabfdd2ab7c575284f6b4646f7b
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-18 00:13:16 +0000

    Jul 18, 2025, 1:13 AM

--- apps/client/app/index.tsx
+++ apps/client/app/index.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'expo-router';
 import { Border, Space } from '@/constants';
 import { useThemeColors } from '@/hooks/useThemeColors';
 import { Action, Heading, Box, Text, Safe, Overlay } from '@/components';
-import { Auth } from '@/features/auth';
 // import { useDeleteAccessToken } from '@/hooks/useTokens';
 
 const images = [
@@ -28,15 +27,8 @@ const images = [
 
 const Index = () => {
   const router = useRouter();
-  const { auth } = Auth.useAuth();
   const colors = useThemeColors();
   // useDeleteAccessToken();
-
-  React.useEffect(() => {
-    if (auth.isLoading && auth.refreshToken) {
-      router.replace('/(tabs)');
-    }
-  }, [auth.refreshToken, auth.isLoading]);
 
   return (
     <Box
