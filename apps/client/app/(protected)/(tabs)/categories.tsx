e99edd7e20f6b2070176b9ee7c6253e4e74ca0a7
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-22 23:58:27 +0000

    Jul 23, 2025, 12:58 AM

--- apps/client/app/(protected)/(tabs)/categories.tsx
+++ apps/client/app/(protected)/(tabs)/categories.tsx
@@ -5,72 +5,68 @@ import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
 import { Categories } from '@/features/categories';
-import { useCategories } from '@/features/categories';
 import { useThemeColors } from '@/hooks/useThemeColors';
 import { Box, Heading, Scroll, Safe } from '@/components';
 
 const CategoriesPage = () => {
   const colors = useThemeColors();
   const router = useRouter();
-  const { setCategories } = useCategories();
 
   return (
-    <React.Fragment>
-      <Safe
-        bg='bg.subtle'
+    <Safe
+      bg='bg.subtle'
+      style={{ flex: 1 }}
+    >
+      <Scroll
         style={{ flex: 1 }}
+        showsVerticalScrollIndicator={false}
       >
-        <Scroll
-          style={{ flex: 1 }}
-          showsVerticalScrollIndicator={false}
+        <Box
+          px='xl'
+          mt='5xl'
         >
-          <Box
-            px='xl'
-            mt='5xl'
-          >
-            <Heading size='4xl'>Categories</Heading>
-          </Box>
+          <Heading size='4xl'>Categories</Heading>
+        </Box>
 
-          <Box
-            px='3xl'
-            mt='4xl'
-            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+        <Box
+          px='3xl'
+          mt='4xl'
+          style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+        >
+          <Heading
+            size='lg'
+            leading='base'
           >
-            <Heading
-              size='lg'
-              leading='base'
+            Recent
+          </Heading>
+
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => {
+              router.push('/(protected)/(categories)/create');
+            }}
+          >
+            <Box
+              bg='bg.base'
+              style={{ padding: 6, borderRadius: Border.radius.full }}
             >
-              Recent
-            </Heading>
+              <Ionicons
+                size={14}
+                name='pencil'
+                color={colors.getColor('icon.inactive')}
+              />
+            </Box>
+          </TouchableOpacity>
+        </Box>
 
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() => {
-                router.push('/(protected)/(categories)/create');
-              }}
-            >
-              <Box
-                bg='bg.base'
-                style={{ padding: 6, borderRadius: Border.radius.full }}
-              >
-                <Ionicons
-                  size={14}
-                  name='pencil'
-                  color={colors.getColor('icon.inactive')}
-                />
-              </Box>
-            </TouchableOpacity>
-          </Box>
-
-          <Box
-            px='xl'
-            style={{ marginTop: Space['xl'] }}
-          >
-            <Categories.List />
-          </Box>
-        </Scroll>
-      </Safe>
-    </React.Fragment>
+        <Box
+          px='xl'
+          style={{ flex: 1, marginTop: Space['xl'] }}
+        >
+          <Categories.List />
+        </Box>
+      </Scroll>
+    </Safe>
   );
 };
 
