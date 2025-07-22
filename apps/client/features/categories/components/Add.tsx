610c9e37948d93204cc0822f0629058bbb5e15c6
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-22 23:44:01 +0000

    Jul 23, 2025, 12:44 AM

--- apps/client/features/categories/components/Add.tsx
+++ apps/client/features/categories/components/Add.tsx
@@ -5,13 +5,18 @@ import { useForm } from 'react-hook-form';
 import { categorySchema } from '../schemas';
 import { useLocalSearchParams } from 'expo-router';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Scroll, Safe, Box, Action } from '@/components';
+import { Scroll, Safe, Box, Action, Heading, Text } from '@/components';
+import { useCreateCategory } from '../hooks/useCreateCategory';
+import { useUpdateCategory } from '../hooks/useUpdateCategory';
+import { Space } from '@/constants';
 
 type AddProps = {
   category?: Category;
 };
 
 export const Add = ({ category }: AddProps) => {
+  const { mutate: createCategory, status: createStatus } = useCreateCategory();
+  const { mutate: updateCategory, status: updateStatus } = useUpdateCategory();
   const { categoryId } = useLocalSearchParams<{ categoryId?: string }>();
   const form = useForm({
     mode: 'all',
@@ -22,12 +27,28 @@ export const Add = ({ category }: AddProps) => {
     },
   });
 
+  const title = categoryId ? 'Update category' : 'Add category';
+  const subTitle = categoryId
+    ? 'Update category details to keep your information accurate and up-to-date for better estimate management.'
+    : "Create a new category to easily manage and organize all your farm's information and estimates in a single, convenient location.";
+
+  const isPending = createStatus === 'pending' || updateStatus === 'pending';
+
   return (
     <Safe>
       <Scroll style={{ flex: 1 }}>
-        <Box></Box>
+        <Box style={{ gap: Space['xl'] }}>
+          <Heading
+            size='3xl'
+            leading='lg'
+            style={{ maxWidth: 280 }}
+          >
+            {title}
+          </Heading>
+          <Text color='text.soft'>{subTitle}</Text>
+        </Box>
 
-        <Box>
+        <Box px='xl'>
           <Form />
         </Box>
       </Scroll>
@@ -35,7 +56,21 @@ export const Add = ({ category }: AddProps) => {
         px='xl'
         mt='4xl'
       >
-        <Action.Root size='xl'>
+        <Action.Root
+          size='xl'
+          loading={isPending}
+          disabled={isPending || !form.formState.isValid}
+          onPress={form.handleSubmit(async (values) => {
+            if (categoryId) {
+              await updateCategory({
+                categoryId,
+                ...values,
+              });
+              return;
+            }
+            await createCategory(values);
+          })}
+        >
           <Action.Loader />
           <Action.Label
             size='lg'
