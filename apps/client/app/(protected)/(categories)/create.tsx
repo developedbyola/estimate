1c41bc3386608bc5ec0c0aac771c26fcf6fff522
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-23 00:08:16 +0000

    Jul 23, 2025, 1:08 AM

--- apps/client/app/(protected)/(categories)/create.tsx
+++ apps/client/app/(protected)/(categories)/create.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Categories } from '@/features/categories';
 
 const CreateCategoryPage = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Categories.Add />
+    </React.Fragment>
+  );
 };
 
 export default CreateCategoryPage;
--- apps/client/features/categories/index.tsx
+++ apps/client/features/categories/index.tsx
@@ -1,10 +1,13 @@
 import { List } from './components/List';
 import { Form } from './components/Form';
-import { useCategories, Provider, Category } from './components/Provider';
+import { useCategories, Provider } from './components/Provider';
+import { Add } from './components/Add';
+import { Category } from './types';
 
 const Categories = {
   Provider,
   Form,
+  Add,
   List,
 };
 
