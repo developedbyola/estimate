73a25491265461c8d16f809b14c13f7957a2aa62
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-23 00:07:11 +0000

    Jul 23, 2025, 1:07 AM

--- apps/client/app/(protected)/(categories)/_layout.tsx
+++ apps/client/app/(protected)/(categories)/_layout.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Stack } from 'expo-router';
+import { router, Stack } from 'expo-router';
+import { Button } from 'react-native';
 
 const CategoriesLayout = () => {
   return (
     <Stack>
       <Stack.Screen
         name='create'
-        options={{ title: 'Set up category', presentation: 'formSheet' }}
+        options={{
+          title: 'Set up category',
+          headerLeft: () => (
+            <Button
+              title='Back'
+              onPress={() => router.back()}
+            />
+          ),
+        }}
       />
     </Stack>
   );
