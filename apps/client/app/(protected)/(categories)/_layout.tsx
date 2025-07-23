48528c3af2dad923f7caaf4b26811bf93f1d4c04
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-23 02:27:43 +0000

    Jul 23, 2025, 3:27 AM

--- apps/client/app/(protected)/(categories)/_layout.tsx
+++ apps/client/app/(protected)/(categories)/_layout.tsx
@@ -3,6 +3,7 @@ import { Border } from '@/constants';
 import { router, Stack } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { useThemeColors } from '@/hooks/useThemeColors';
+import { TouchableOpacity } from 'react-native';
 
 const CategoriesLayout = () => {
   const colors = useThemeColors();
@@ -14,16 +15,16 @@ const CategoriesLayout = () => {
         options={{
           title: 'Set up category',
           headerLeft: () => (
-            <TouchableOpacity style={{
-              height: 32,
-              aspectRatio: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: Border.radius.full,
-              backgroundColor: colors.getColor('bg.soft'),
-
-            }}>
-
+            <TouchableOpacity
+              style={{
+                height: 32,
+                aspectRatio: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: Border.radius.full,
+                backgroundColor: colors.getColor('bg.soft'),
+              }}
+            >
               <Ionicons
                 size={20}
                 name='chevron-back'
