2e25f26a1d36a19741877be930378153083aac57
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-24 12:45:29 +0000

    Jul 24, 2025, 1:45 PM

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,5 +1,8 @@
 import { betterAuth } from 'better-auth';
+import { Pool } from 'pg';
 
 export const auth = betterAuth({
-  database: 'postgresql://postgres:postgres@localhost:5432/postgres',
+  database: new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
 });
