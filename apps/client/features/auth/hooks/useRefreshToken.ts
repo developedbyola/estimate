0b9a71e1fb656ca33ba559338311a18d5bed0449
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-26 08:55:15 +0000

    Jul 26, 2025, 9:55 AM

--- apps/client/features/auth/hooks/useRefreshToken.ts
+++ apps/client/features/auth/hooks/useRefreshToken.ts
@@ -1,18 +1,12 @@
-import { useAuth } from '@/contexts/authContext';
-import { refreshToken } from '@/lib/auth';
+import React from 'react';
+import { useAuth } from '../components/Provider';
 
 export const useRefreshToken = () => {
-  const data = useAuth();
+  const { refetch } = useAuth();
 
-  // const mutate = refreshToken(
-  //   { userId: session?.user.id },
-  //   {
-  //     onSuccess: (data) => {
-  //       setAuth({ type: 'REFRESH_TOKEN' });
-  //     },
-  //     onError: (err) => {
-  //       setAuth({ type: 'ERROR' });
-  //     },
-  //   }
-  // );
+  React.useEffect(() => {
+    refetch();
+  }, []);
+
+  return {};
 };
