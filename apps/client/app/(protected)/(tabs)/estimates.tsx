ccaeba4142e24900f68949562e69a3ede929be8f
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-26 15:57:02 +0000

    Jul 26, 2025, 4:57 PM

--- apps/client/app/(protected)/(tabs)/estimates.tsx
+++ apps/client/app/(protected)/(tabs)/estimates.tsx
@@ -1,7 +1,26 @@
+import { Box, Heading, Safe } from '@/components';
 import React from 'react';
 
 const EstimatesPage = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Safe
+        bg='bg.base'
+        style={{ flex: 1 }}
+      >
+        <Box
+          px='xl'
+          mt='5xl'
+        >
+          <Heading
+            style={{ fontSize: 40, fontWeight: '600', letterSpacing: -0.5 }}
+          >
+            Estimates
+          </Heading>
+        </Box>
+      </Safe>
+    </React.Fragment>
+  );
 };
 
 export default EstimatesPage;
