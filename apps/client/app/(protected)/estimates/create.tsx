4e752fb328d3c05f2cc6af35384d7453624ddb40
developedbyola <olanrewajuoladejo08@gmail.com>
2025-07-27 07:34:34 +0000

    Jul 27, 2025, 8:34 AM

--- apps/client/app/(protected)/estimates/create.tsx
+++ apps/client/app/(protected)/estimates/create.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Estimates } from '@/features/estimates';
 
 const CreateEstimatePage = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Estimates.Add />
+    </React.Fragment>
+  );
 };
 
 export default CreateEstimatePage;
